6a9fd168168a10fab07609bdc73d5cddf917ae56
Fernando <fernando.alvarez@islas.org.mx>
2021-01-27 15:59:21 -0800

    🎨 Apply black to format scripts

--- tests/test_mapping.py
+++ tests/test_mapping.py
@@ -2,6 +2,7 @@ from nerd.mapping import safe_divition
 from unittest import TestCase
 import numpy as np
 
+
 class TestMapping(TestCase):
     def setUp(self) -> None:
         self.a = 60
@@ -9,7 +10,7 @@ class TestMapping(TestCase):
         self.c = 0
 
     def test_safe_divition(self):
-        expected = 60/2
+        expected = 60 / 2
         obtained = safe_divition(self.a, self.b)
         assert expected == obtained
 
