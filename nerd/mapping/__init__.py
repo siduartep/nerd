e8901e77fc7c7dc3ddd8a1b127c0e8c207aba0ee
Fernando <fernando.alvarez@islas.org.mx>
2021-02-15 12:10:36 -0800

    🎨 Fix mapping __init__ format

--- nerd/mapping/__init__.py
+++ nerd/mapping/__init__.py
@@ -1,7 +1,7 @@
-from .tiling import (
+from .tiling import (  # noqa
     safe_divition,
     slope_between_two_points,
     orthogonal_slope,
     slopes_from_coordinates,
     generate_tile_from_coordinates,
-)  # noqa
+)
