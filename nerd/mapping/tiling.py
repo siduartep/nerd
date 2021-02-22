e70c046e1382f6adcb068237601d074834d43998
Fernando <fernando.alvarez@islas.org.mx>
2021-02-22 12:21:34 -0800

    ⚡️🐛🔥 Fix bug in x tiles indices

--- nerd/mapping/tiling.py
+++ nerd/mapping/tiling.py
@@ -30,15 +30,15 @@ def calculate_tile_y_limits(slope, limit_x, x_coord, y_coord):
 
 
 def tile_y_coordinates(start_orthogonal_slope, end_orthogonal_slope, X_rect, X, Y, node):
-    startY1 = calculate_tile_y_limits(start_orthogonal_slope, X_rect[1], X[node], Y[node])
-    startY2 = calculate_tile_y_limits(start_orthogonal_slope, X_rect[2], X[node], Y[node])
-    endY2 = calculate_tile_y_limits(end_orthogonal_slope, X_rect[3], X[node + 1], Y[node + 1])
-    endY1 = calculate_tile_y_limits(end_orthogonal_slope, X_rect[4], X[node + 1], Y[node + 1])
+    startY1 = calculate_tile_y_limits(start_orthogonal_slope, X_rect[0], X[node], Y[node])
+    startY2 = calculate_tile_y_limits(start_orthogonal_slope, X_rect[1], X[node], Y[node])
+    endY2 = calculate_tile_y_limits(end_orthogonal_slope, X_rect[2], X[node + 1], Y[node + 1])
+    endY1 = calculate_tile_y_limits(end_orthogonal_slope, X_rect[3], X[node + 1], Y[node + 1])
     return [startY1, startY2, endY2, endY1, startY1]
 
 
 def calculate_tile_x_limits(r, orthogonal_slope, x_coord):
-    return r / np.sqrt(orthogonal_slope ** 2 + 1) + x_coord
+    return r / np.sqrt(1 + orthogonal_slope ** 2) + x_coord
 
 
 def tile_x_coordinates(r, start_orthogonal_slope, end_orthogonal_slope, X, node):
