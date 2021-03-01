a22e47c5c8ef84948d9398075a662b7d6a16b177
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-03-01 10:51:27 -0800

    🎨 Rename variables using snake_case instead of CamelCase

--- nerd/mapping/tiling.py
+++ nerd/mapping/tiling.py
@@ -15,14 +15,14 @@ def safe_divition(numerator, denominator):
     return numerator / denominator
 
 
-def cell_edges_slopes(X, Y, node_index):
+def cell_edges_slopes(x, y, node_index):
     start_slope = orthogonal_slope(
         slope_between_two_points(
-            Y[node_index + 1], Y[node_index - 1], X[node_index + 1], X[node_index - 1]
+            y[node_index + 1], y[node_index - 1], x[node_index + 1], x[node_index - 1]
         )
     )
     end_slope = orthogonal_slope(
-        slope_between_two_points(Y[node_index + 2], Y[node_index], X[node_index + 2], X[node_index])
+        slope_between_two_points(y[node_index + 2], y[node_index], x[node_index + 2], x[node_index])
     )
     return start_slope, end_slope
 
@@ -31,29 +31,29 @@ def calculate_cell_y_limits(slope, limit_x, x_coord, y_coord):
     return slope * (limit_x - x_coord) + y_coord
 
 
-def cell_y_coordinates(start_orthogonal_slope, end_orthogonal_slope, X_rect, X, Y, node):
-    startY1 = calculate_cell_y_limits(start_orthogonal_slope, X_rect[0], X[node], Y[node])
-    startY2 = calculate_cell_y_limits(start_orthogonal_slope, X_rect[1], X[node], Y[node])
-    endY2 = calculate_cell_y_limits(end_orthogonal_slope, X_rect[2], X[node + 1], Y[node + 1])
-    endY1 = calculate_cell_y_limits(end_orthogonal_slope, X_rect[3], X[node + 1], Y[node + 1])
-    return [startY1, startY2, endY2, endY1, startY1]
+def cell_y_coordinates(start_orthogonal_slope, end_orthogonal_slope, x_rect, x, y, node):
+    start_y1 = calculate_cell_y_limits(start_orthogonal_slope, x_rect[0], x[node], y[node])
+    start_y2 = calculate_cell_y_limits(start_orthogonal_slope, x_rect[1], x[node], y[node])
+    end_y2 = calculate_cell_y_limits(end_orthogonal_slope, x_rect[2], x[node + 1], y[node + 1])
+    end_y1 = calculate_cell_y_limits(end_orthogonal_slope, x_rect[3], x[node + 1], y[node + 1])
+    return [start_y1, start_y2, end_y2, end_y1, start_y1]
 
 
 def calculate_cell_x_limits(r, orthogonal_slope, x_coord):
     return r / np.sqrt(1 + orthogonal_slope ** 2) + x_coord
 
 
-def cell_x_coordinates(r, start_orthogonal_slope, end_orthogonal_slope, X, node):
-    startX1 = calculate_cell_x_limits(r, start_orthogonal_slope, X[node])
-    startX2 = calculate_cell_x_limits(-r, start_orthogonal_slope, X[node])
-    endX1 = calculate_cell_x_limits(r, end_orthogonal_slope, X[node + 1])
-    endX2 = calculate_cell_x_limits(-r, end_orthogonal_slope, X[node + 1])
-    return [startX1, startX2, endX2, endX1, startX1]
+def cell_x_coordinates(r, start_orthogonal_slope, end_orthogonal_slope, x, node):
+    start_x1 = calculate_cell_x_limits(r, start_orthogonal_slope, x[node])
+    start_x2 = calculate_cell_x_limits(-r, start_orthogonal_slope, x[node])
+    end_x1 = calculate_cell_x_limits(r, end_orthogonal_slope, x[node + 1])
+    end_x2 = calculate_cell_x_limits(-r, end_orthogonal_slope, x[node + 1])
+    return [start_x1, start_x2, end_x2, end_x1, start_x1]
 
 
-def generate_cell_from_coordinates(X, Y, node, stripe_width, spatial_resolution):
+def generate_cell_from_coordinates(x, y, node, stripe_width, spatial_resolution):
     r = stripe_width / 2
-    start_orthogonal_slope, end_orthogonal_slope = cell_edges_slopes(X, Y, node)
-    X_rect = cell_x_coordinates(r, start_orthogonal_slope, end_orthogonal_slope, X, node)
-    Y_rect = cell_y_coordinates(start_orthogonal_slope, end_orthogonal_slope, X_rect, X, Y, node)
-    return X_rect, Y_rect
+    start_orthogonal_slope, end_orthogonal_slope = cell_edges_slopes(x, y, node)
+    x_rect = cell_x_coordinates(r, start_orthogonal_slope, end_orthogonal_slope, x, node)
+    y_rect = cell_y_coordinates(start_orthogonal_slope, end_orthogonal_slope, x_rect, x, y, node)
+    return x_rect, y_rect
