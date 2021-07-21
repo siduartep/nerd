bb1c092ff8f10fe0c55cc0c21dc0d012cfdbdf95
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-07-21 15:51:42 -0700

    ✅ Update and pass test

---
+++ nerd/io/import_data.py
@@ -0,0 +1,11 @@
+import collections
+import pandas as pd
+
+column_names = ["date", "time", "Lat", "Lon", "Speed", "heading", "Logging_on", "altitude"]
+
+
+def import_tracmap(input, output):
+    tracmap_data = pd.read_csv(
+        input, header=None, names=column_names, usecols=[i for i in range(1, 9)]
+    )
+    tracmap_data.to_csv(output, index=False)
