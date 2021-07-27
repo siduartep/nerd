01d5f1ee0fc9fc89ee719f3e09e254a1c96e3a46
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-07-27 16:02:37 -0700

    🗺️ Transform geografic coordinates to UTM coordinates

--- nerd/io/geo2utm.py
+++ nerd/io/geo2utm.py
@@ -1,10 +1,10 @@
 import pandas as pd
-
-column_names = ["date", "time", "Lat", "Lon", "Speed", "heading", "Logging_on", "altitude"]
+import utm
 
 
 def geo2utm(input):
-    tracmap_data = pd.read_csv(
-        input, header=None, names=column_names, usecols=[i for i in range(1, 9)]
+    data = pd.read_csv(input)
+    data["easting"], data["northing"], data["zone_number"], data["zone_letter"] = utm.from_latlon(
+        data.Lat.values, data.Lon.values
     )
-    tracmap_data.to_csv(output, index=False)
+    return data
