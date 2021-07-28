87230787ed54c3673f9030c09d0fbfde271118bb
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-07-28 16:06:04 -0700

    🚚 Rename variables

--- nerd/io/geo2utm.py
+++ nerd/io/geo2utm.py
@@ -2,8 +2,8 @@ import pandas as pd
 import utm
 
 
-def geo2utm(input):
-    data = pd.read_csv(input)
+def geo2utm(csv_filename):
+    data = pd.read_csv(csv_filename)
     data["easting"], data["northing"], data["zone_number"], data["zone_letter"] = utm.from_latlon(
         data.Lat.values, data.Lon.values
     )
--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -6,7 +6,7 @@ from pandas._testing import assert_frame_equal
 def test_tracmap2csv():
     expected_csv = pd.read_csv("tests/data/expected_input_data.csv")
     tracmap2csv(
-        input="tests/data/tracmap_sample_data.txt", output="tests/data/imported_data.csv"
+        tracmap_filename="tests/data/tracmap_sample_data.txt", csv_filename="tests/data/imported_data.csv"
     )
     obtained_csv = pd.read_csv("tests/data/imported_data.csv")
     assert_frame_equal(expected_csv, obtained_csv)
