c3b7103c79f6544889507c798ce4d5028bfe94cf
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2021-09-06 12:35:13 -0700

    ✅ Tests argument csv_filename

--- tests/test_import_data.py
+++ tests/test_import_data.py
@@ -1,3 +1,4 @@
+import os
 import pandas as pd
 from nerd.io import import_tracmap, tracmap2csv
 from pandas._testing import assert_frame_equal
@@ -13,7 +14,15 @@ def test_tracmap2csv():
     assert_frame_equal(expected_csv, obtained_csv)
 
 
-def test_import_tracmap():
+def test_import_tracmap_without_csvfilename():
     expected_utm_data = pd.read_csv("tests/data/expected_utm_data.csv")
     obtained_utm_data = import_tracmap(tracmap_filename="tests/data/tracmap_sample_data.txt")
     assert_frame_equal(expected_utm_data, obtained_utm_data)
+    os.remove("input_data.csv")
+
+def test_import_tracmap_with_csvfilename():
+    test_csv_filename = "test_csv_filename.csv"
+    expected_utm_data = pd.read_csv("tests/data/expected_utm_data.csv")
+    obtained_utm_data = import_tracmap(tracmap_filename="tests/data/tracmap_sample_data.txt", csv_filename=test_csv_filename)
+    os.path.isfile(test_csv_filename)
+    os.remove(test_csv_filename)
