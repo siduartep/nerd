d5d0ffb8962224f5b255d479d292018be31876b5
Evaristo Rojas <evaristo.rojas@islas.org.mx>
2022-02-26 12:05:40 -0800

    🔧 Update config file for PyPI

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 from setuptools import setup, find_packages
 
 setup(
+    author="Ciencia de Datos • GECI",
+    author_email="ciencia.datos@islas.org.mx",
+    description="Numerical Estimation of Rodenticide Density (NERD)",
     name="geci-nerd",
-    version="0.3.1",
     packages=find_packages(),
+    url="https://github.com/IslasGECI/nerd",
+    version="0.3.1",
     install_requires=[
         "descartes",
         "fiona",
